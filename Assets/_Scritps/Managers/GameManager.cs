e7546eb0ce55a10bcde8a5eb24046c1febf45e21
jpdoyle96 <jpdoyle96@gmail.com>
2022-10-08 13:04:17 -0400

    Created framework of GameManager class

--- Assets/_Scritps/Managers/GameManager.cs
+++ Assets/_Scritps/Managers/GameManager.cs
@@ -1,22 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class GameManager : Singleton<MonoBehaviour>
+public class GameManager : Singleton<GameManager>
 {
-    [field: SerializeField]
+    public static event Action<GameState> OnBeforeStateChanged;
+    public static event Action<GameState> OnAfterStateChanged;
+
+    [field: SerializeField]  
     internal Player Player { get; private set; }
 
+    public GameState State { get; private set; }
 
-    // Start is called before the first frame update
-    void Start()
+    void Start() => ChangeState(GameState.Start);
+
+    public void ChangeState(GameState newState)
     {
-        
+        if (newState != State)
+        {
+            OnBeforeStateChanged?.Invoke(newState);
+
+            State = newState;
+            switch(newState)
+            {
+                case GameState.Start:
+                    HandleStart();
+                    break;
+                case GameState.Home:
+                    HandleHome();
+                    break;
+                case GameState.Spawn:
+                    HandleSpawn();
+                    break;
+                case GameState.Playing:
+                    HandlePlaying();
+                    break;
+                case GameState.GameOver:
+                    HandleGameOver();
+                    break;
+                case GameState.Pause:
+                    HandlePause();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+            }
+        }
     }
 
-    // Update is called once per frame
+    private void HandleStart() { }
+
+    private void HandleHome() { }
+
+    private void HandleSpawn() { }
+
+    private void HandlePlaying() { }
+
+    private void HandleGameOver() { }
+
+    private void HandlePause() { }
+
+    // Core Game Loop
     void Update()
     {
-        
+    
     }
+}
+
+[Serializable]
+public enum GameState
+{
+    Start = 0,
+    Home = 1,
+    Spawn = 2,
+    Playing = 3,
+    GameOver = 4,
+    Pause = 5
 }
